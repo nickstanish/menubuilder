e265d9830dcce6f7e33a8bceef2882d54520252c
Nick Stanish <nickstanish@gmail.com>
2014-03-25 00:44:55 -0400

    Can now add and delete sections through KO, improved appearance by 10%

--- public/javascripts/menubuilder.js
+++ public/javascripts/menubuilder.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
-    ko.applyBindings({
-        sections: [
-            { title: 'title1', text: 'text1' },
-            { title: 'title2', text: 'text2' },
-            { title: 'title3', text: 'text3' }
+function AppModel() {
+	var self = this;
+ 	self.ids = 1;
+    self.sections = ko.observableArray([
+            { title: 'title1', text: 'text1' , id: self.ids++},
+            { title: 'title2', text: 'text2' , id: self.ids++},
+            { title: 'title3', text: 'text3' , id: self.ids++}
         ]
-    });
-});
+    );
  
+    self.addSection = function() {
+        self.sections.push({ title: "title" + self.ids, text: "text" + self.ids, id: self.ids++ });
+    };
+ 
+    self.removeSection = function(section) {
+        self.sections.remove(section);
+    }
+}
+
+$(document).ready(function() {
+    ko.applyBindings( new AppModel());
+});
