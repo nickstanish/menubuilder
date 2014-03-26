98fb0d3fccde0f88cee7615251d3012aa5e8bc2a
Nick Stanish <nickstanish@gmail.com>
2014-03-26 15:37:25 -0400

    added knockout switch library, enabled deleting of menuitems, separated sections, enabled editing of section and menuitem titles, made prettier ;)

--- public/javascripts/menubuilder.js
+++ public/javascripts/menubuilder.js
@@ -1,31 +1,56 @@
 function MenuItem(){ // default model
     var self = this;
-    this.serviceType        = "Haircut";
-    this.title              = "Men's Haircut";
-    this.description        = "The best haircuts for men on the planet!";
-    this.regularPrice       = 50;
-    this.appointmentLength  = 90;
+    self.serviceType        = ko.observable("Haircut");
+    self.title              = ko.observable("Men's Haircut");
+    self.description        = ko.observable("The best haircuts for men on the planet!");
+    self.regularPrice       = ko.observable(0);
+    self.appointmentLength  = ko.observable(90);
+
+}
+function Section(id){
+    var self = this;
+    self.title = ko.observable("Hair Services");
+    self.id = id
+    self.menuItems = ko.observableArray();
+    self.addMenuItem = function(){
+            self.menuItems.push( new MenuItem());
+        }
+    self.removeMenuItem = function(item){
+            self.menuItems.remove(item);
+        }
+    self.addMenuItem();
 
 }
 function AppModel() {
 	var self = this;
  	self.ids = 1;
+    self.dataStatus    = ko.observable(0); 
+    self.statusSection = 1;
+    self.statusItem    = 2;
     self.selection = ko.observable();   // used in form
-    self.sections = ko.observableArray([ // switch up later
-            { title: 'title1', text: 'text1' , id: self.ids++, menuItems: ko.observableArray([new MenuItem(),new MenuItem(),new MenuItem()]) },
-            { title: 'title2', text: 'text2' , id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) },
-            { title: 'title3', text: 'text3' , id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) }
-        ]
-    );
- 
+    self.sections = ko.observableArray();
     self.addSection = function() { 
-        self.sections.push({ title: "title" + self.ids, text: "text" + self.ids, id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) });
+        self.sections.push(new Section(self.ids++));
     };
+    self.addSection();
+    self.addSection();
     self.removeSection = function(section) {
+        self.dataStatus(0);
         self.sections.remove(section);
     }
-    self.setSelection = function(item){
+    self.setStatusSection = function(item){
+        self.dataStatus(self.statusSection);
         self.selection(item);
+    }
+    self.setStatusItem = function(item){
+        self.dataStatus(self.statusItem);
+        self.selection(item);
+    }
+    self.removeItem = function(item, parent){
+        console.log(item);
+        console.log(parent);
+        parent.menuItems().remove(item);
+        return false;
     }
 }
 
