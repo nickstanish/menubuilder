7b5ef768a7a74e51b620821f9f06b2ae8b0e30dc
Nick Stanish <nickstanish@gmail.com>
2014-03-26 00:13:05 -0400

    Added basic form for menuitems, added display for menuitems in list, changed accordian display into a sidebar that is responsive friendly, fixed glyphicon fonts for bootstrap, turned on auto complete evolution scripts

--- public/javascripts/menubuilder.js
+++ public/javascripts/menubuilder.js
@@ -1,19 +1,31 @@
+function MenuItem(){ // default model
+    var self = this;
+    this.serviceType        = "Haircut";
+    this.title              = "Men's Haircut";
+    this.description        = "The best haircuts for men on the planet!";
+    this.regularPrice       = 50;
+    this.appointmentLength  = 90;
+
+}
 function AppModel() {
 	var self = this;
  	self.ids = 1;
-    self.sections = ko.observableArray([
-            { title: 'title1', text: 'text1' , id: self.ids++},
-            { title: 'title2', text: 'text2' , id: self.ids++},
-            { title: 'title3', text: 'text3' , id: self.ids++}
+    self.selection = ko.observable();   // used in form
+    self.sections = ko.observableArray([ // switch up later
+            { title: 'title1', text: 'text1' , id: self.ids++, menuItems: ko.observableArray([new MenuItem(),new MenuItem(),new MenuItem()]) },
+            { title: 'title2', text: 'text2' , id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) },
+            { title: 'title3', text: 'text3' , id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) }
         ]
     );
  
-    self.addSection = function() {
-        self.sections.push({ title: "title" + self.ids, text: "text" + self.ids, id: self.ids++ });
+    self.addSection = function() { 
+        self.sections.push({ title: "title" + self.ids, text: "text" + self.ids, id: self.ids++, menuItems: ko.observableArray([new MenuItem()]) });
     };
- 
     self.removeSection = function(section) {
         self.sections.remove(section);
+    }
+    self.setSelection = function(item){
+        self.selection(item);
     }
 }
 
